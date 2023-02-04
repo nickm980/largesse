3aae9b0f6f5de85503d060ba1561d50aa52b3d00
Michael Fehrer <mtfehrer@gmail.com>
2023-02-04 17:20:38 -0500

    sdfsdf

---
+++ app/src/csvParser.js
@@ -0,0 +1,14 @@
+fetch("path/to/file.csv")
+    .then((response) => response.text())
+    .then((csvData) => {
+        const csvRows = csvData.split("\n");
+        const csvArray = [];
+
+        for (const row of csvRows) {
+            const columns = row.split(",");
+            csvArray.push({
+                name: columns[0],
+                address: columns[1],
+            });
+        }
+    });
