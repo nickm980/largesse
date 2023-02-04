cafc9c094668e09013909cfabfdb3df54a021070
Michael Fehrer <mtfehrer@gmail.com>
2023-02-04 16:41:00 -0500

    sdfsdf

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar">
+        <span>About</span>
+        <span>Shelters</span>
+        <span>More Stuff</span>
+    </nav>
+</template>
+
+<style>
+.navbar {
+    display: flex;
+    gap: 1rem;
+}
+</style>
