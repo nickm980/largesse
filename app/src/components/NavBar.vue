c74b5ee19430cb7801d51f2a1ce5078fd42d5f94
nickm980 <81270095+nickm980@users.noreply.github.com>
2023-02-04 17:45:40 -0500

    a

---
+++ app/src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav class="navbar">
+        <div class="logo">
+            <p id="logoimg">LARGESSE</p>
+        </div>
+
+        <ul class="links">
+            <li><router-link>Food</router-link></li>
+            <li>Shelter</li>
+            <li>Clothing</li>
+            <li>Community</li>
+            <li>Animals</li>
+        </ul>
+        <div class="spacer"></div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    props: {
+        msg: String,
+    },
+};
+</script>
+
+<style>
+#logoimg {
+    font-weight: bold;
+}
+.navbar {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+}
+.logo {
+    flex: .2;
+}
+
+.links {
+    margin: 0 auto;
+    flex: 1;
+}
+.spacer {
+    flex: .2;
+}
+
+li {
+    float: left;
+    margin-left: 1em;
+    margin-right: 1em;
+}
+
+ul {
+    list-style: none;
+}
+</style>
