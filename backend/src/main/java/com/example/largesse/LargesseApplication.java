b6b4e299504de2c890e06bc7fb88bdf83dba27c2
Michael Fehrer <mtfehrer@gmail.com>
2023-10-08 19:52:12 -0400

    added backend and docker-compose config

---
+++ backend/src/main/java/com/example/largesse/LargesseApplication.java
@@ -0,0 +1,13 @@
+package com.example.largesse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargesseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargesseApplication.class, args);
+	}
+
+}
