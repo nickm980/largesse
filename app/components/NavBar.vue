76bbe4c4c922d59f9811348ff3475731755e81c6
Michael Fehrer <mfehrer@redhat.com>
2024-01-07 17:16:57 -0500

    added navbar categories

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -3,11 +3,7 @@
         <NuxtLink to="/">
             <img id="image" src="/static/logo.jpg" />
         </NuxtLink>
-        <Dropdown
-            v-for="item in data"
-            :title="item.title"
-            :content="item.content"
-        />
+        <Dropdown v-for="item in data" :title="item.title" :content="item.content" />
     </div>
 </template>
 
@@ -17,35 +13,45 @@ const data = [
         title: "Environment",
         content: [
             { title: "Climate Change", link: "/events/climate" },
-            { title: "test2", link: "link2" },
+            { title: "Farm Work", link: "/events/farming" },
+            { title: "Environmental Conservation", link: "/events/conservation" },
+            { title: "Permaculture Projects", link: "/events/permaculture" },
         ],
     },
     {
         title: "Animals",
         content: [
-            { title: "test1", link: "link1" },
-            { title: "test2", link: "link2" },
+            { title: "Animal Farms", link: "/events/farming" },
+            { title: "Wildlife Conservation", link: "/events/conservation" },
+            { title: "Animal Rescue", link: "/events/rescue" },
+            { title: "Animal Care", link: "/events/care" },
         ],
     },
     {
         title: "Social",
         content: [
-            { title: "test1", link: "link1" },
-            { title: "test2", link: "link2" },
+            { title: "Volunteering with Young People", link: "young" },
+            { title: "Teaching and Education", link: "education" },
+            { title: "Community Development", link: "community" },
+            { title: "Women's Equity and Gender Empowerment", link: "gender" },
         ],
     },
     {
         title: "Health Care",
         content: [
-            { title: "test1", link: "link1" },
-            { title: "test2", link: "link2" },
+            { title: "Volunteering with the Elderly", link: "elderly" },
+            { title: "Counseling", link: "counseling" },
+            { title: "Holistic Centers", link: "holistic" },
+            { title: "Clinics", link: "clinics" },
         ],
     },
     {
         title: "Other",
         content: [
-            { title: "test1", link: "link1" },
-            { title: "test2", link: "link2" },
+            { title: "Mental Health", link: "mental" },
+            { title: "Food Pantry", link: "food" },
+            { title: "Shelters", link: "shelters" },
+            { title: "Clothing", link: "clothing" },
         ],
     },
 ];
